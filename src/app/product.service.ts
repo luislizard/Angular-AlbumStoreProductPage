f42b5a099bfa1db5e209d84f35308904a8aa365d
luislizard <55631672+luislizard@users.noreply.github.com>
2020-07-30 15:12:00 -0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  _albumUrl = '../assets/album.json';
+  private _albumUrl:string = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
